e059b534ab2bb9f17b32f1989fa5afa1167e82a2
Supun Induwara <supun.i@ustocktrade.com>
2018-08-20 13:01:02 +0530

    changes

--- src/main/java/com/example/rest/Main.java
+++ src/main/java/com/example/rest/Main.java
@@ -34,12 +34,14 @@ public class Main {
      * @param args
      * @throws IOException
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
+       while (true) {
+           Thread.sleep(60000);
+       }
+        //server.stop();
     }
 }
 
